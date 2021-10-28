36945b4b51c4edb4df508593bc7970c0a14bb805
NIHILncunia <nihil_ncunia@naver.com>
2021-10-28 14:14:18 +0900

    2021 1028 템플릿 업데이트

--- build/Events/ready.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.event = void 0;
-exports.event = {
-    name: 'ready',
-    run: (client) => {
-        console.log(`[상태] '${client.user.username} - v${client.config.version}' 활성화 완료.`);
-    },
-};
